1b00db559f450699ea64d07a32d85e482ccba349
Sergio Medeiros <sergiof11@outlook.com.br>
2023-01-10 21:00:14 -0300

    Criação da página e opacity em todos os elementos ao iniciar a tela

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function () {
+  opacityAll();
+  button();
+});
+
+function opacityAll() {
+  $(".grid-container").css("opacity", "0.2");
+}
+
+function button() {
+  $("button").css("opacity", "0.9");
+}
